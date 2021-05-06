14265135843d82215c1042430104c7dea780f730
Rithvik Doshi <rithvik@bu.edu>
2021-05-05 23:26:26 -0400

    Bottle Fill Only

---
+++ Bottle-Fill-Only.java
@@ -0,0 +1,12 @@
+public class BottleFillOnly{
+
+	public static void main(String[] args){
+		System.out.println("Bottle Fill Only!\n");
+		System.out.println("Bottle Fill Only Bottle Fill Only Bottle Fill Only, Bottle Fill Only.");
+		System.out.println("Bottle Fill Only, Bottle Fill Only Bottle Fill Only Bottle Fill Only?");
+		System.out.println("Bottle Fill Only! Bottle Fill Only Bottle Fill Only! Bottle Fill Only Bottle Fill Only.");
+		System.out.println("\nBottle Fill Only,");
+		System.out.println("Bottle Fill Only.");
+	}
+
+}
