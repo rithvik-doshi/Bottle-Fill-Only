c856397bed4920f08d9b96b1ecc7e163bab4f830
Rithvik Doshi <rithvik@bu.edu>
2021-05-05 23:13:46 -0400

    Bottle Fill Only

---
+++ Bottle-Fill-Only.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Bottle Fill Only\n\n";
+  std::cout << "Bottle Fill Only. Bottle Fill Only, Bottle Fill Only Bottle Fill Only Bottle Fill Only\nBottle Fill Only Bottle Fill Only Bottle Fill Only.\n";
+  std::cout << "Bottle Fill Only Bottle Fill Only, Bottle Fill Only. Bottle Fill Only Bottle Fill Only? Bottle Fill Only Bottle Fill Only Bottle Fill Only!\n";
+  std::cout << "Bottle Fill Only, Bottle Fill Only Bottle Fill Only Bottle Fill Only Bottle Fill Only? Bottle Fill Only Bottle Fill Only Bottle Fill Only!\n";
+  std::cout << "\nBottle Fill Only,\nBottle Fill Only.\n";
+  
+  return 0;
+}
